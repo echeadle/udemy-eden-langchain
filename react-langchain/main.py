23016c6972782ca16fe6cf9d3b3283e66462e058
echeadle <echeadle@gmail.com>
2024-10-12 09:21:49 -0600

    Finshed lesson 29

--- react-langchain/main.py
+++ react-langchain/main.py
@@ -1,12 +1,13 @@
 from typing import Union, List
 
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import AgentAction, AgentFinish
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.tools import Tool, tool
+from langchain.tools import Tool
 from langchain.tools.render import render_text_description
 
 
@@ -19,7 +20,7 @@ def get_text_length(text: str) -> int:
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip(
         '"'
-    )  # stripping away non alphabetic characters just in case
+    )  # stripping away non-alphabetic characters just in case
 
     return len(text)
 
@@ -28,55 +29,45 @@ def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
             return tool
-    raise ValueError(f"Tool with name {tool_name} not found")
+    raise ValueError(f"Tool wtih name {tool_name} not found")
 
 
-if __name__ == '__main__':
-    print("Hello React LangChain!")
+if __name__ == "__main__":
+    print("Hello ReAct LangChain!")
     tools = [get_text_length]
 
     template = """
     Answer the following questions as best you can. You have access to the following tools:
 
     {tools}
-
-    Use the following format:
-
-    Question: the input question you must answer
-
-    Thought: you should always think about what to do
-
-    Action: the action to take, should be one of [{tool_names}]
-
-    Action Input: the input to the action
-
-    Observation: the result of the action
-
-    ... (this Thought/Action/Action Input/Observation can repeat N times)
-
-    Thought: I now know the final answer
-
-    Final Answer: the final answer to the original input question
-
-    Begin!
-
-    Question: {input}
-    Thought:  {agent_scratchpad}
-    """
     
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools),
         tool_names=", ".join([t.name for t in tools]),
     )
 
-
     llm = ChatOpenAI(
         temperature=0,
-        model="gpt-4o-mini",
-        stop=["\nObservation", "Observation"],
+        stop= ["\nObservation", "Observation"],
     )
     intermediate_steps = []
-    
     agent = (
         {
             "input": lambda x: x["input"],
@@ -87,20 +78,28 @@ if __name__ == '__main__':
         | ReActSingleInputOutputParser()
     )
 
-    
     agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
         {
             "input": "What is the length in characters of the text DOG",
             "agent_scratchpad": intermediate_steps,
         }
     )
-    
     print(agent_step)
     if isinstance(agent_step, AgentAction):
         tool_name = agent_step.tool
         tool_to_use = find_tool_by_name(tools, tool_name)
         tool_input = agent_step.tool_input
-        
         observation = tool_to_use.func(str(tool_input))
-        print(observation)
-        intermediate_steps.append(agent_step, )
+        print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of the word: DOG",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+    print(agent_step)
+    if isinstance(agent_step, AgentFinish):
+        print("### AgentFinish ###")
+        print(agent_step.return_values)
